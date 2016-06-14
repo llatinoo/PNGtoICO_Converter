edb900e8e3d67dcd63ab001e1c8fe302de8a5ad0
Michael Alves Pereira <hello@leer>
2016-06-14 11:06:47 +0200

    Initial Commit: Read input of directory

---
+++ PNGtoICO_Converter/PNGtoICO_Converter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PNGtoICO_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"I:\Filme";
+            string newfilm = "";
+            var films = Directory.GetDirectories(path);
+
+            foreach (string film in films)
+            {
+                if (film.Contains(path))
+                    newfilm = film.Replace(path + "\\", "");
+                Console.WriteLine(newfilm);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
